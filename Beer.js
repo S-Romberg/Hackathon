5f2558d203052580be98ea297ed1e0e09cc12e41
S Romberg <SRomberg@Macbook-Pro.local>
2018-04-27 12:43:42 -0600

    getting there

--- Beer.js
+++ Beer.js
@@ -1,4 +1,7 @@
 const beerAPI = "https://api.punkapi.com/v2/beers"
+var descList = [];
+
+
 fetch(beerAPI) 
 .then(response => response.json())
 .then(sortData)
@@ -6,21 +9,22 @@ fetch(beerAPI)
 
 function sortData(data){
     description(data) 
-    beerName()
+    // beerName()
+    chooseDescription()
     
 }
 function description(data){
   var description = data.map(newData => {
     var beerDesco = document.createElement("p")
     beerDesco.setAttribute("class", newData.name)
-      newData.description
-      
-      
+    beerDesco.textContent = newData.description
+    descList.push(beerDesco)
   }) 
-  
-  
-  
-  
-      
-  
+}
+function chooseDescription(something){
+    return document.getElementById('description').textContent = descList[getRandomInt(25)]
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
