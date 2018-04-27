5288cf4b030d65ab4e8eb327cd6baf54bbc19f65
S Romberg <SRomberg@Macbook-Pro.local>
2018-04-27 13:37:43 -0600

    reactions

--- Beer.js
+++ Beer.js
@@ -1,6 +1,11 @@
 const beerAPI = "https://api.punkapi.com/v2/beers"
+const drinkGif = 'http://api.giphy.com/v1/gifs/search?q=drink&api_key=Jt5TZH39Ei7DGga7jzbeddDvoU0FugCm&limit=10'
+
+
 var descList = [];
 var buttons = [1,2,3]
+var empty = []
+
 console.log(descList)
 fetch(beerAPI) 
 .then(response => response.json())
@@ -33,17 +38,40 @@ function getRandomInt(max) {
 }
 
 function populateButtons(pTag){
-  console.log(pTag.classList.value);
   let correct = document.createElement('button')
+
   correct.textContent = document.getElementById('chosen').classList
-  console.log(document.getElementById('chosen').classList)
   document.getElementById('buttons').appendChild(correct)
+  correct.classList += 'beerButton'
+  correct.addEventListener('click', handleCorrect)
+
   var buttonContent = buttons.forEach(yes => {
     var createButton = document.createElement('button')
     createButton.classList += 'beerButton'
     createButton.textContent = descList[getRandomInt(25)].classList.value
     document.getElementById('buttons').appendChild(createButton)
-    console.log(createButton.textContent)
+    createButton.addEventListener('click', handleWrong)
   })
   
+}
+
+function handleCorrect(event){
+  console.log('yay')
+}
+function handleWrong(event){
+  fetch(drinkGif)
+  .then(function(response){
+      return response.json();
+  })
+  .then(function(response){
+      console.log(response.data);
+      var gif = response.data.map(object => {
+          var iframe = document.createElement('iframe')
+          iframe.setAttribute('frameBorder', '0')
+          iframe.src = object.embed_url
+          console.log(iframe.src)
+          return empty.push(iframe)
+      })
+  })
+  console.log(empty)
 }
