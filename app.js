3efa1bea90f27c4e3c2914aa571c704f7c8b5366
S Romberg <SRomberg@Macbook-Pro.local>
2018-04-27 11:35:59 -0600

    finally

---
+++ app.js
@@ -0,0 +1,15 @@
+const apiURL = "http://api.giphy.com/v1/gifs/search?q=kids+falling+over&api_key=Jt5TZH39Ei7DGga7jzbeddDvoU0FugCm&limit=5"
+
+fetch(apiURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response.data);
+        var gif = response.data.map(object => {
+            var iframe = document.createElement('iframe')
+            iframe.setAttribute('frameBorder', '0')
+            document.getElementById('body').appendChild(iframe)
+            return iframe.src = object.embed_url
+        })
+    })
